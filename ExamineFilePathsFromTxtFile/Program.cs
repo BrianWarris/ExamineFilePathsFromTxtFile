c2585910d6dd191f05982a825ecf153d8499f478
Brian Warris <bwy7@comcast.net>
2018-02-13 09:08:46 -0800

    changes to show that only FileInfo constructor throws a PathTooLongException on 260  or more characters; File.Exists does not. It just returns false;

--- ExamineFilePathsFromTxtFile/Program.cs
+++ ExamineFilePathsFromTxtFile/Program.cs
@@ -8,13 +8,42 @@ using System.Threading.Tasks;
 
 namespace ExamineFilePathsFromTxtFile
 {
+    delegate  bool FileDetectMethod(string filePath);
     class Program
     {
+        static bool DetectFileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+        static bool DetectFileInfoExists(string filePath)
+        {
+            FileInfo fi = new FileInfo(filePath);
+            return ((fi != null) && (fi.Exists));
+        }
+        static bool DetermineFileExistence(string nextPath, FileDetectMethod fn, string methodName)
+        {
+            bool foundIt = false;
+            try
+            {
+                foundIt = fn(nextPath);
+            }
+            catch (System.ArgumentException)
+            {
+                Debug.WriteLine($"Detected an ArgumentException on {nextPath.Length} characters when calling {methodName}");
+            }
+            catch (PathTooLongException)
+            {
+                Debug.WriteLine($"Detected a PathTooLongException on {nextPath.Length} characters when calling {methodName}");
+            }
+            return foundIt;
+        }
+
         static void Main(string[] args)
         {
             // overall goal: if path to txt file was passed, then open it and iterate through the contents, commenting on file existence,
             //               else echo the syntax of the app
             bool showSyntax = false;
+            List<string> lstChars = new List<string>();
             string nonBlockSpace = new string(new char[] { (char)0xfffd });
 
             if (((args == null) || (args.Length == 0)) || (args[0] == @"\?"))
@@ -42,33 +71,16 @@ namespace ExamineFilePathsFromTxtFile
                                         {
                                             nextPath = nextPath.Replace("\"", "");
                                         }
-                                        foreach (char ch in nextPath)
-                                        {
-                                            int chi = (int)ch;
-                                            Debug.Write(string.Format("{0}", chi.ToString("X")));
-                                        }
-                                        Debug.WriteLine(Environment.NewLine);
                                         if (nextPath.Contains(nonBlockSpace))
                                         {
                                             nextPath = nextPath.Replace(nonBlockSpace, "");
                                         }
                                         if (nextPath.Trim().Length > 0)
                                         {
-                                            FileInfo fif = null;
-                                            bool foundIt = false;
-                                            try
-                                            {
-                                                fif = new FileInfo(nextPath);
-                                                foundIt = (bool)fif.Exists;
-                                            }
-                                            catch (System.ArgumentException)
-                                            {
-                                                Debug.WriteLine($"Detected an ArgumentException on {nextPath}");
-                                            }
-                                            catch (PathTooLongException)
-                                            {
-                                                Debug.WriteLine($"Detected a PathTooLongException on {nextPath}");
-                                            }
+                                            FileDetectMethod fdm = DetectFileExists;
+                                            bool foundIt = DetermineFileExistence(nextPath, fdm, "File.Exists");
+                                            fdm = DetectFileInfoExists;
+                                            foundIt = DetermineFileExistence(nextPath, fdm, "new Filenfo") && foundIt;
                                             string comment = (foundIt) ? " " : " NOT ";
                                             Console.Out.WriteLine($"\tFile {nextPath} was{comment}found.");
                                         }
